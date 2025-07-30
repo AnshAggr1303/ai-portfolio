1c4096f5003a7636e437f7c8658d277b8e35aa01
AnshAggr1303 <103252458+AnshAggr1303@users.noreply.github.com>
2025-07-30 22:48:37 +0530

    sort UI

--- app/components/ui/skills.tsx
+++ app/components/ui/skills.tsx
@@ -125,7 +125,7 @@ const Skills = () => {
       transition={{ duration: 0.6, ease: [0.19, 1, 0.22, 1] as const }}
       className="mx-auto w-full max-w-5xl rounded-4xl"
     >
-      <Card className="w-full border-none px-0 pb-12 shadow-none">
+      <Card className="w-full border-none px-0 pb-12 shadow-none bg-transparent">
         <CardHeader className="px-0 pb-1">
           <CardTitle className="text-black px-0 text-4xl font-bold">Skills & Expertise</CardTitle>
         </CardHeader>
